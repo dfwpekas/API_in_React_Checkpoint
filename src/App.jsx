ba8d6400e83467fdca2731346c6e42a298bce754
dfwpekas <chiemerienwafor88@gmail.com>
2024-10-16 15:20:55 +0100

    API in React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UsersList from "./UsersList";
+
+function App() {
+
+  return (
+    <>
+      <UsersList/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/UsersList.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const UsersList = () => {
+    const [listofuser, setListofuser] = useState([])
+
+
+// fetch data from the server using axios in useEffects hook
+
+    useEffect(() => {
+        axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((res) => {setListofuser(res.data)}) // saving fetched data into state.
+            .catch((error) => {
+                console.error("Error Fetching data", error);
+            });
+    }, []);
+
+return (
+        <div className="user-list">
+            <h1>List of Users</h1>
+            <ul>
+                {listofuser.map((user) => (
+                    <li key={user.id}> UserName: {user.name}
+                    <hr />
+                    Email: {user.email}
+                    <hr />
+                    </li>
+                ))};
+            </ul>
+        </div>
+    )
+}
+ 
+export default UsersList;
